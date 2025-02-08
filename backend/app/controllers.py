1bca6969c7cf092daba89b85313cfa8d2acb3f29
Enrique Álvarez Mesa <enrique.avz.msa@gmail.com>
2025-02-08 17:36:35 +0000

    feat(backend): added POST endpoint to save images

--- backend/app/controllers.py
+++ backend/app/controllers.py
@@ -1,18 +1,53 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, UploadFile
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models import Image
+from pathlib import Path
+import shutil
+from PIL import Image as PILImage
 
 router = APIRouter()
+
+UPLOAD_DIR = Path("/app/uploads")
+
 
 @router.get("/imagenes")
 def obtener_imagenes(db: Session = Depends(get_db)):
     return db.query(Image).all()
 
-@router.post("/imagenes")
-def crear_imagen(nombre: str, url: str, db: Session = Depends(get_db)):
-    nueva_imagen = Image(nombre=nombre, url=url)
-    db.add(nueva_imagen)
-    db.commit()
-    db.refresh(nueva_imagen)
-    return nueva_imagen
+
+def get_image_metadata(file):
+    """ Obtiene dimensiones y tipo MIME de la imagen """
+    file_size = len(file.file.read())  # Tamaño en bytes
+    img = PILImage.open(file.file)
+    width, height = img.size
+    file_type = img.format.lower()
+    return width, height, file_size, file_type
+
+
+@router.post("/upload/")
+def upload_image(file: UploadFile, description = "", 
+                 db: Session = Depends(get_db)):
+    image_path = UPLOAD_DIR / file.filename
+    width, height, file_size, file_type = get_image_metadata(file)
+    try:
+        with image_path.open("wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+    except Exception as e:
+        return {"error": f"Error al guardar el archivo: {str(e)}"}
+
+    new_image = Image(
+        name=file.filename,
+        path=str(image_path),
+        file_size=file_size,
+        file_type=file_type,
+        width=width,
+        height=height
+    )
+    try:
+        db.add(new_image)
+        db.commit()
+        db.refresh(new_image)
+    except:
+        return {"error": "Error al guardar la imagen en la BBDD"}
+    return {"message": "Imagen subida correctamente."}
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,10 +1,17 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, TIMESTAMP
 from app.database import Base  # Importamos la base para definir modelos
+from datetime import datetime, timezone
 
 # Modelo para la tabla de imágenes
 class Image(Base):
     __tablename__ = "images"
 
-    id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String, nullable=False)
-    url = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    path = Column(String, nullable=False)  # Ruta en el sistema de archivos
+    description = Column(String, nullable=True)
+    upload_date = Column(TIMESTAMP, default=datetime.now(timezone.utc) )
+    file_size = Column(Integer, nullable=True)  # Tamaño en bytes
+    file_type = Column(String, nullable=True)  # Tipo de imagen (MIME)
+    width = Column(Integer, nullable=True)  # Ancho en píxeles
+    height = Column(Integer, nullable=True)  # Alto en píxeles
